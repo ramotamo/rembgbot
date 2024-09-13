1398e051863dc9bbc4f864378273704a3b5a74d4
ramotamo <ramotamo19@gmail.com>
2024-09-13 21:12:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from rembg import remove
 # Replace 'YOUR_BOT_TOKEN' with your bot token
 BOT = telebot.TeleBot('7238717600:AAF2ReLPIxItfqltUsTJddCnqiJ10mH14kA')
 
-@bot.message_handler(content_types=['photo'])
+@BOT.message_handler(content_types=['photo'])
 def handle_photo(message):
     file_info = BOT.get_file(message.photo[-1].file_id)
     downloaded_file = BOT.download_file(file_info.file_path)
